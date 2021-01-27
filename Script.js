365e2e1ca2a33ed5d5f781a0a7e57921d2986fcf
Miranda Hinchliffe <70153183+MirandaHinchliffe@users.noreply.github.com>
2021-01-27 12:49:14 -0500

    Delete Script.js

--- Script.js
+++
@@ -1,7 +0,0 @@
-let dog = {
-    type: "Sparky",
-    numLegs: 4,
-    sayLegs: function(){ return "This dog has " + this.numLegs + " legs."}
-};
-console.log(dog.sayLegs());
-document.getElementById("spark").innerHTML = "Sparky has 4 legs"; 
